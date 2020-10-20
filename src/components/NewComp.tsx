d8116ca50634e92d0c00dce0b413065091b19b36
Logan Wood <lwood3499@gmail.com>
2020-10-19 21:37:58 -0500

    first commit

---
+++ src/components/NewComp.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewComponent = () => {
+
+
+  return (
+    <div>
+      This is a new component that I've created.
+    </div>
+  );
+}
+
+export default NewComponent;
